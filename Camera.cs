5fb3bf1fb2ceb32dfe6a6051d8eccafc0d6e83fe
PSILVA <131625628+PAndrade22@users.noreply.github.com>
2023-09-05 13:22:14 -0300

    Create Camera.cs

---
+++ Camera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CamaraController : MonoBehaviour {
+public GameObject player;
+private Vector3 deslocamento;
+void Start () {
+deslocamento = transform.position;
+}
+void Update () {
+transform.position = player.transform.position + deslocamento;
+}
+}
