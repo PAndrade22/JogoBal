719916bc0d44887335754db680bcaea38d6ac691
PSILVA <131625628+PAndrade22@users.noreply.github.com>
2023-09-13 13:40:36 -0300

    Update MoveBall.cs

--- MoveBall.cs
+++ MoveBall.cs
@@ -12,11 +12,11 @@ public float jumpforce = 2;
 
 public bool isGrounded;
 
-public Text winText;
-
 public float velocidade;
 
 private int count = 0;
+
+public Text winText;
 
 public Text countText;
 
@@ -30,10 +30,10 @@ void OnTriggerEnter(Collider other) {
 }
 
 void SetCountText() {
-countText.text = "Count: " + count.ToString();
-if (count >= 5){
-    winText.text = "YOU WIN!";
-}
+    countText.text = "Count: " + count.ToString();
+    if (count >= 7) {
+        winText.text = "YOU WIN!";
+    }
 }
 
 
@@ -49,19 +49,10 @@ void OnCollisionStay(){
 }
 
 void Update () {
-    float movimentoHorizontal = 0.0f;
-    float movimentoVertical = 0.0f;
-
-    if (usarTeclasWSAD) {
-        movimentoHorizontal = Input.GetKey(KeyCode.D) ? 1 : Input.GetKey(KeyCode.A) ? -1 : 0;
-        movimentoVertical = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
-    } else {
-        movimentoHorizontal = Input.GetAxis("Horizontal");
-        movimentoVertical = Input.GetAxis("Vertical");
-    }
-
-    Vector3 movimento = new Vector3(movimentoHorizontal, 0.0f, movimentoVertical);
-    rb.AddForce(movimento * velocidade);
+float movimentoHorizontal = Input.GetAxis("Horizontal");
+float movimentoVertical = Input.GetAxis("Vertical");
+Vector3 movimento = new Vector3(movimentoHorizontal, 0.0f, movimentoVertical);
+rb.AddForce(movimento * velocidade);
 
  if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
         rb.AddForce(jump * jumpforce, ForceMode.Impulse);
